f5d94ffbced1b963ab804561e266ed140bbc0240
Alex <muhinalex77@gmail.com>
2021-04-04 17:51:57 +0300

    carusel and media add

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/right.png"></button>',
+        slidesToShow: 1,
+        adaptiveHeight: false,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
