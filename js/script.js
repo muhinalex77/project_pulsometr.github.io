843e2c1fb080221ea76e0e318f04e0fe015be8ed
Alex <muhinalex77@gmail.com>
2021-04-08 18:24:47 +0300

    add pageup

--- js/script.js
+++ js/script.js
@@ -152,4 +152,14 @@ $(document).ready(function() {
         });
         return false;
     });
+    // smooth scroll and page up
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
 });
